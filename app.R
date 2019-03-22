47fd8c303d420d168b925b14a208e01c69e458e6
sjsj0101 <songjie1217@hotmail.com>
2019-03-22 18:08:40 +0800

    notext

--- app.R
+++ app.R
@@ -4,42 +4,30 @@ library(ggvis)
 
 ui <- fluidPage(
   titlePanel("It's ALIVE"),
-  
-  sidebarLayout(
-    sidebarPanel(
+  fluidRow(
+    column(3,wellPanel(
       sliderInput("season", "season",
                   1, 4, 1, step = 1),
       sliderInput("episode", "episode", 
                   1, 10, 1,step =1)
-    ),
-    mainPanel(
-      imageOutput("image"),
-      textOutput("text")
-    ))
-)
+    )),
+    column(3,imageOutput("image")),
+    column(2,textOutput("text"))
+  
+))
 
 server <- function(input, output) {
   output$image <- renderImage({
-    if (input$season == 1 & input$episode == 1){
-      return(list(
-        src = "www/s1e1.jpg",
+      list(
+        src = paste("www/s",input$season,"e",input$episode,".jpg",sep=""),
         filetype = "image/jpeg",
         width = 600,
         height = 400,
         alt = "s1e1"
-      ))
-    } 
-    else if (input$season == 1 & input$episode == 2){
-      return(list(
-        src = "www/s1e2.jpg",
-        filetype = "image/jpeg",
-        width = 600,
-        height = 400,
-        alt = "s1e2"
-      ))
-    }
-
+      )
   },deleteFile = FALSE)
+  
+  
 }
 
 shinyApp(ui = ui, server = server)
